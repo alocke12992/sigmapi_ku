9ebd829c6815b015a8f217d06ec8eece523a8bc9
Andrew Locke <alocke12992@gmail.com>
2019-05-13 20:37:01 -0700

    Join page styles

--- src/pages/join.js
+++ src/pages/join.js
@@ -1,16 +1,28 @@
 import React from 'react';
 import Construction from 'components/Construction';
 import { Container, Section } from 'styledComponents';
+import { media } from 'styledComponents/theme';
 import { LinkButton } from 'styledComponents/Button';
+
+const forms = {
+  "recForm": "https://chapterbuilder.com/forms/?id=6820&chapter=7bd64c3f2c75f525116cea96bd07ae94",
+  "highSchoolInterestForm": "https://chapterbuilder.com/forms/?id=6803&chapter=7bd64c3f2c75f525116cea96bd07ae94",
+  "collegeStudentInterestForm": "https://chapterbuilder.com/forms/?id=6839&chapter=7bd64c3f2c75f525116cea96bd07ae94"
+}
 const recForm = "https://chapterbuilder.com/forms/?id=6820&chapter=7bd64c3f2c75f525116cea96bd07ae94"
 const interestForm = "https://chapterbuilder.com/forms/?id=6803&chapter=7bd64c3f2c75f525116cea96bd07ae94"
 import styled from 'styled-components';
 
 const Row = styled(Container)`
   display: flex;
-  flex-direction: row;
+  flex-direction: column;
   justify-content: space-around;
   align-items: flex-start;
+
+  ${media.tablet`
+      flex-direction: row;
+      max-width: 660px;
+  `}
 `;
 
 const Column = styled(Container)`
@@ -21,6 +33,20 @@ const Column = styled(Container)`
   a {
     margin-top: 1.5rem;
   }
+
+  .interest-forms {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    text-align: center;
+    width: 100%;
+    max-width: 330px;
+
+    ${media.tablet`
+      flex-direction: row;
+      max-width: 660px;
+    `}
+  }
 `
 
 
@@ -29,8 +55,11 @@ const ContactPage = () => (
   <Section>
     <Column>
     <h1>Join Sigma Pi</h1>
-    <p>Sigma Pi is interested in recruiting well-rounded, impressive men from all over the country. We invite high school seniors, freshmen college students and others interested in knowing more about Sigma Pi at the University of Kansas to contact our Recruitment Chairman, Nolan Sell, by clicking on the button below. We will be sure to contact you to answer any questions you may have about our fraternity and keep you informed of upcoming rush events, and we appreciate your interest in Sigma Pi.</p>
-    <LinkButton to={interestForm}>Interest Form</LinkButton>
+    <p>Sigma Pi is interested in recruiting well-rounded, impressive men from all over the country. We invite high school seniors, freshmen college students and others interested in knowing more about Sigma Pi at the University of Kansas to contact our Recruitment Chairman, Nolan Sell, by clicking on one of the buttons below. We will be sure to contact you to answer any questions you may have about our fraternity and keep you informed of upcoming rush events, and we appreciate your interest in Sigma Pi.</p>
+    <div className="interest-forms">
+      <LinkButton to={forms["highSchoolInterestForm"]}>High School Senior</LinkButton>
+      <LinkButton to={forms["collegeStudentInterestForm"]}>Current College Student</LinkButton>
+    </div>
     </Column>
   </Section>
   <Section dark>
@@ -62,12 +91,12 @@ const ContactPage = () => (
       <div>
         <h3>Rush</h3>
         <ul>
-          <li><strong>Saturday, May 25th, 4:00pm-6:00pm:</strong><br/>Game day @ the Sigma Pi house</li>
-          <li><strong>Saturday, June 1st, 5:00pm-7:00pm</strong><br/>Basketball @ the Sigma Pi house</li>
-          <li><strong>Saturday June 8th, 2:00pm - 4:00pm: </strong><br/>Campus tour/ House tour @ the Sigma Pi house</li>
-          <li><strong>Saturday June 15th, TBD</strong><br/>NBA Playoffs Watch Party @ the Sigma Pi house</li>
+          <li><strong>Saturday, May 25th<br />4:00pm-6:00pm:</strong><br/>Game day @ the Sigma Pi house</li>
+          <li><strong>Saturday, June 1st<br />5:00pm-7:00pm:</strong><br/>Basketball @ the Sigma Pi house</li>
+          <li><strong>Saturday June 8th<br />2:00pm-4:00pm:</strong><br/>Campus tour/ House tour @ the Sigma Pi house</li>
+          <li><strong>Saturday June 15th<br />TBD</strong><br/>NBA Playoffs Watch Party @ the Sigma Pi house</li>
           <li><strong>Friday June 21st thru Sunday June 23rd:</strong><br/>Formal Rush</li>
-          <li><strong>Saturday June 28th, 4pm</strong><br/>Sand Volleyball/Cookout @ the Sigma Pi house</li>
+          <li><strong>Saturday June 28th<br />4pm</strong><br/>Sand Volleyball/Cookout @ the Sigma Pi house</li>
         </ul>
       </div>
     </Row>
