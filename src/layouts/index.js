00015fada4406498c69c467916ae347132bbf3fa
Andrew Locke <alocke12992@gmail.com>
2020-01-28 09:23:49 -0800

    Remove undefined

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,28 +3,12 @@ import { ThemeProvider } from 'styled-components'
 import theme from 'styledComponents/theme';
 import 'styles/base.css';
 import Navbar from 'components/Navbar';
-import Footer from 'components/Footer';
 
-const Layout = ({ children, location }) => {
-  console.log("test", name)
-  return (
+const Layout = ({ children, location }) => (
   <ThemeProvider theme={theme}>
     <>
       <Navbar location={location} />
       {children}
-      {/* <Footer /> */}
     </>
   </ThemeProvider>
-)};
-
-export default Layout
-
-export const query = graphql`
-  query {
-    president: contentfulPerson(position: { eq: "President"}){
-      name
-      email
-      phone
-    }
-  }
-`;
+);
